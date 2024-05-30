ab92f5d173abb5aa1f72e978a164bf4145b7e3e5
Derrick Franco <dfranc3373@gmail.com>
2024-05-30 16:27:17 +0200

    Endpoints for test

--- core/views.py
+++ core/views.py
@@ -12,36 +12,5 @@ class CityModelViewSet(ModelViewSet):
 
 
 class EarthquakeSearchViewSet(ViewSet):
-    def find_existing_search_result(self, validated_data):
-        """
-        Find an existing search result by city, start date, and end date.
-        """
-        return SearchResult.objects.get(**validated_data)
-
-    def get_search_result(self, data):
-        """
-        Gets the search result based on the given data.
-        """
-        existing_search_result = self.find_existing_search_result(data)
-
-        if existing_search_result:
-            return existing_search_result
-
-        service = EarthquakeService()
-        # TODO IMPLEMENT
-        # new_search_result = service.get_nearest_earthquake(data)
-
-        return new_search_result
-
-    def list(self, request):
-        """
-        Receives a GET request, validates received data, fetches earthquake data,
-        finds the nearest earthquake, saves the result and returns it.
-        """
-        serializer = SearchResultSerializer(data=request.GET)
-        serializer.is_valid(raise_exception=True)
-
-        search_result = self.get_search_result(serializer.validated_data)
-
-        serializer = SearchResultSerializer(search_result)
-        return Response(serializer.data)
+    # TODO Build out endpoints
+    pass
